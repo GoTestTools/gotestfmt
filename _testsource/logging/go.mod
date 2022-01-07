89dd20951f1cadbd909141d85770e379eb112de8
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-01-07 12:38:39 +0100

    Fixes #30: Test output formatter

---
+++ _testsource/logging/go.mod
@@ -0,0 +1,7 @@
+module github.com/haveyoudebuggedit/example
+
+go 1.17
+
+require k8s.io/klog/v2 v2.40.1
+
+require github.com/go-logr/logr v1.2.0 // indirect
--- renderer/renderer.go
+++ renderer/renderer.go
@@ -2,8 +2,13 @@ package renderer
 
 import (
 	"bytes"
+	"context"
 	"fmt"
+	"os/exec"
+	"runtime"
+	"strings"
 	"text/template"
+	"time"
 
 	"github.com/haveyoudebuggedit/gotestfmt/v2/parser"
 )
@@ -93,9 +98,51 @@ type Package struct {
 	Settings RenderSettings
 }
 
+func formatTestOutput(testOutput string, cfg RenderSettings) string {
+	if cfg.Formatter == "" {
+		return testOutput
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	var shell []string
+	if runtime.GOOS == "windows" {
+		shell = []string{
+			"cmd.exe",
+			"/C",
+			cfg.Formatter,
+		}
+	} else {
+		shell = []string{
+			"/bin/bash",
+			"-c",
+			cfg.Formatter,
+		}
+	}
+
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+
+	run := exec.CommandContext(ctx, shell[0], shell[1:]...)
+	run.Stdin = bytes.NewReader([]byte(testOutput))
+	run.Stdout = stdout
+	run.Stderr = stderr
+	if err := run.Run(); err != nil {
+		panic(fmt.Errorf(
+			"failed to run test output formatter '%s', stderr was: %s (%w)",
+			strings.Join(shell, " "),
+			stderr.String(),
+			err,
+		))
+	}
+	return stdout.String()
+}
+
 func renderTemplate(templateName string, templateText []byte, data interface{}) []byte {
 	result := bytes.Buffer{}
 	tpl := template.New(templateName)
+	tpl.Funcs(map[string]interface{}{
+		"formatTestOutput": formatTestOutput,
+	})
 	tpl, err := tpl.Parse(string(templateText))
 	if err != nil {
 		panic(fmt.Errorf("failed to parse template (%w)", err))
@@ -118,4 +165,6 @@ type RenderSettings struct {
 	HideSuccessfulTests bool
 	// ShowTestStatus adds words to indicate the test status next to the icons (PASS, FAIl, SKIP).
 	ShowTestStatus bool
+	// Formatter is the path to an external program that is executed for each test output for format it.
+	Formatter string
 }
